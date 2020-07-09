846639d419edbbcd495b3bd166b073ddeea801bb
Ritvik Nimmagadda <47041400+RitvikN18@users.noreply.github.com>
2020-07-09 23:44:35 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -30,34 +30,33 @@ class VideoCamera(object):
         # releasing camera
         self.video.release()
 
+    def get_frame(self):
+        # extracting frames
+        ret, test_img = self.video.read()
+        gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+        faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+        for (x, y, w, h) in faces_detected:
+            cv2.rectangle(test_img, (x, y), (x+w, y+h), (255, 0, 0), thickness=7)
+            roi_gray = gray_img[y:y+w, x:x+h]
+            roi_gray = cv2.resize(roi_gray, (48, 48))
+            img_pixels = image.img_to_array(roi_gray)
+            img_pixels = np.expand_dims(img_pixels, axis=0)
+            img_pixels /= 255.0
 
-def get_frame(self):
-    # extracting frames
-    ret, test_img = self.video.read()
-    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
-    for (x, y, w, h) in faces_detected:
-        cv2.rectangle(test_img, (x, y), (x+w, y+h), (255, 0, 0), thickness=7)
-        roi_gray = gray_img[y:y+w, x:x+h]
-        roi_gray = cv2.resize(roi_gray, (48, 48))
-        img_pixels = image.img_to_array(roi_gray)
-        img_pixels = np.expand_dims(img_pixels, axis=0)
-        img_pixels /= 255.0
+            predictions = model.predict(img_pixels)
 
-        predictions = model.predict(img_pixels)
+            max_index = np.argmax(predictions[0])
 
-        max_index = np.argmax(predictions[0])
+            emotions = ('angry', 'disgust', 'fear', 'happy',
+                        'sad', 'surprise', 'neutral')
+            predicted_emotion = emotions[max_index]
 
-        emotions = ('angry', 'disgust', 'fear', 'happy',
-                    'sad', 'surprise', 'neutral')
-        predicted_emotion = emotions[max_index]
-
-        cv2.putText(test_img, predicted_emotion, (int(x), int(y)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    # encode OpenCV raw frame to jpg and displaying it
-    resized_img = cv2.resize(test_img, (1000, 700))
-    ret, jpeg = cv2.imencode('.jpg', resized_img)
-    return jpeg.tobytes()
+            cv2.putText(test_img, predicted_emotion, (int(x), int(y)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        # encode OpenCV raw frame to jpg and displaying it
+        resized_img = cv2.resize(test_img, (1000, 700))
+        ret, jpeg = cv2.imencode('.jpg', resized_img)
+        return jpeg.tobytes()
 
 
 # while True:
