9e9d8e34591e17805436724e2023d4791c281997
Ritvik Nimmagadda <47041400+RitvikN18@users.noreply.github.com>
2020-07-09 23:11:25 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,95 @@
+import os
+import sys
+import cv2
+import numpy as np
+import tensorflow as tf
+#import keras
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.utils import CustomObjectScope
+from tensorflow.keras.initializers import glorot_uniform
+
+# with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+
+json_file = open('model.json', 'r')
+loaded_json_model = json_file.read()
+json_file.close()
+model = model_from_json(loaded_json_model)
+model.load_weights("model.h5")
+face_haar_cascade = cv2.CascadeClassifier(
+    'haarcascade_frontalface_default.xml')
+#cap = cv2.VideoCapture(0)
+
+
+class VideoCamera(object):
+    def __init__(self):
+        # capturing video
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        # releasing camera
+        self.video.release()
+
+
+def get_frame(self):
+    # extracting frames
+    ret, test_img = self.video.read()
+    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+    for (x, y, w, h) in faces_detected:
+        cv2.rectangle(test_img, (x, y), (x+w, y+h), (255, 0, 0), thickness=7)
+        roi_gray = gray_img[y:y+w, x:x+h]
+        roi_gray = cv2.resize(roi_gray, (48, 48))
+        img_pixels = image.img_to_array(roi_gray)
+        img_pixels = np.expand_dims(img_pixels, axis=0)
+        img_pixels /= 255.0
+
+        predictions = model.predict(img_pixels)
+
+        max_index = np.argmax(predictions[0])
+
+        emotions = ('angry', 'disgust', 'fear', 'happy',
+                    'sad', 'surprise', 'neutral')
+        predicted_emotion = emotions[max_index]
+
+        cv2.putText(test_img, predicted_emotion, (int(x), int(y)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    # encode OpenCV raw frame to jpg and displaying it
+    resized_img = cv2.resize(test_img, (1000, 700))
+    ret, jpeg = cv2.imencode('.jpg', resized_img)
+    return jpeg.tobytes()
+
+
+# while True:
+#     ret, test_img = cap.read()
+#     if not ret:
+#         continue
+#     gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+
+#     faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+#     for (x, y, w, h) in faces_detected:
+#         cv2.rectangle(test_img, (x, y), (x+w, y+h), (255, 0, 0), thickness=7)
+#         roi_gray = gray_img[y:y+w, x:x+h]
+#         roi_gray = cv2.resize(roi_gray, (48, 48))
+#         img_pixels = image.img_to_array(roi_gray)
+#         img_pixels = np.expand_dims(img_pixels, axis=0)
+#         img_pixels /= 255.0
+
+#         predictions = model.predict(img_pixels)
+
+#         max_index = np.argmax(predictions[0])
+
+#         emotions = ('angry', 'disgust', 'fear', 'happy',
+#                     'sad', 'surprise', 'neutral')
+#         predicted_emotion = emotions[max_index]
+
+#         cv2.putText(test_img, predicted_emotion, (int(x), int(y)),
+#                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+#     resized_img = cv2.resize(test_img, (1000, 700))
+#     cv2.imshow('Facial emotion analysis ', resized_img)
+#     if cv2.waitKey(10) == ord('q'):
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows
